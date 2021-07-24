f3cf75e31dd54efb5272bcbba3bd0d1455ddf8b1
thomas hamer <thomas.hamer@cmd.com.au>
2021-07-24 21:02:14 +1000

    uncomment and update outputs

--- iot_infra/iot.tf
+++ iot_infra/iot.tf
@@ -65,53 +65,53 @@ resource "aws_iot_topic_rule" "rule" {
   sql         = "SELECT * FROM 'topic/esp0001'"
   sql_version = "2016-03-23"
 
-  # sns {
-  #   message_format = "RAW"
-  #   role_arn       = aws_iam_role.role.arn
-  #   target_arn     = aws_sns_topic.mytopic.arn
-  # }
+  sns {
+    message_format = "RAW"
+    role_arn       = aws_iam_role.role.arn
+    target_arn     = aws_sns_topic.mytopic.arn
+  }
 
-  # error_action {
-  #   sns {
-  #     message_format = "RAW"
-  #     role_arn       = aws_iam_role.role.arn
-  #     target_arn     = aws_sns_topic.myerrortopic.arn
-  #   }
-  # }
+  error_action {
+    sns {
+      message_format = "RAW"
+      role_arn       = aws_iam_role.role.arn
+      target_arn     = aws_sns_topic.myerrortopic.arn
+    }
+  }
 }
 
-# resource "aws_sns_topic" "mytopic" {
-#   name = "mytopic"
-#   tags = {
-#       repository = "hamer/iot"
-#       workspace  = terraform.workspace
-#     }
-# }
+resource "aws_sns_topic" "mytopic" {
+  name = "mytopic"
+  tags = {
+      repository = "hamer/iot"
+      workspace  = terraform.workspace
+    }
+}
 
-# resource "aws_sns_topic" "myerrortopic" {
-#   name = "myerrortopic"
-#   tags = {
-#       repository = "hamer/iot"
-#       workspace  = terraform.workspace
-#     }
-# }
+resource "aws_sns_topic" "myerrortopic" {
+  name = "myerrortopic"
+  tags = {
+      repository = "hamer/iot"
+      workspace  = terraform.workspace
+    }
+}
 
-# resource "aws_iam_role_policy" "iam_policy_for_lambda" {
-#   name = "mypolicy"
-#   role = aws_iam_role.role.id
+resource "aws_iam_role_policy" "iam_policy_for_lambda" {
+  name = "mypolicy"
+  role = aws_iam_role.role.id
 
-#   policy = <<EOF
-# {
-#   "Version": "2012-10-17",
-#   "Statement": [
-#     {
-#         "Effect": "Allow",
-#         "Action": [
-#             "sns:Publish"
-#         ],
-#         "Resource": "${aws_sns_topic.mytopic.arn}"
-#     }
-#   ]
-# }
-# EOF
-# }
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+        "Effect": "Allow",
+        "Action": [
+            "sns:Publish"
+        ],
+        "Resource": "${aws_sns_topic.mytopic.arn}"
+    }
+  ]
+}
+EOF
+}
---
+++ iot_infra/output.tf
@@ -0,0 +1,14 @@
+output "certificate" {
+    value = aws_iot_certificate.cert.certificate_pem
+    description = "Certificate.pem file"
+}
+
+output "public_key" {
+    value = aws_iot_certificate.cert.public_key
+    description = "public key file"
+}
+
+output "private_key" {
+    value = aws_iot_certificate.cert.private_key
+    description = "private key file"
+}
