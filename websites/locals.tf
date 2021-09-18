403f2e64553e1e3a5f612e8f6e8ccb9ded80073e
Thomas Hamer <thomas_hamer@outlook.com>
2021-09-18 22:26:45 +1000

    deployed hamer cloud in code with tf

---
+++ websites/locals.tf
@@ -0,0 +1,12 @@
+locals {
+  
+  env = {
+    hamer = {
+      aws_profile     = "hamer"
+      aws_region      = "us-east-1"
+      domain_name     = "hamer.cloud"
+    }
+  }
+
+  workspace = local.env[terraform.workspace]
+}
