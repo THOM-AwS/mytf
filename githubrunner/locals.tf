1db78eb5fd0118af9322b84ff4657e9636a28ce8
Thomas Hamer <thomas_hamer@outlook.com>
2021-09-22 00:16:49 +1000

    incremental to add runner

--- githubrunner/locals.tf
+++ githubrunner/locals.tf
@@ -8,14 +8,11 @@ locals {
       subnet_ids = ["subnet-094eb31fc174ed123", "subnet-060b94669a3c2d1a8", "subnet-05016f77be825ba9b"]
 
       environment = "Prod"
-
-      github_app = {
-        key_base64     = "base64string"
-        id             = "139153"
-        client_id      = "Iv1.23641bb57b3756bb"
-        client_secret  = "client_secret"
-        webhook_secret = "webhook_secret"
-      }
+      key_base64     = "base64string"
+      id             = "139153"
+      client_id      = "Iv1.23641bb57b3756bb"
+      client_secret  = "791c4b724354081187305c7d5888aaa9d913f5db"
+      webhook_secret = "webhook_secret"
     }
   }
   workspace = local.env[terraform.workspace]
--- githubrunner/main.tf
+++ githubrunner/main.tf
@@ -9,15 +9,38 @@ module "github-runner_runners" {
 
   environment = local.workspace["environment"]
 
-  github_app = {
+  github_app_parameters = {
     key_base64     = local.workspace["key_base64"]
     id             = local.workspace["id"]
     client_id      = local.workspace["client_id"]
     client_secret  = local.workspace["client_secret"]
     webhook_secret = local.workspace["webhook_secret"]
   }
-
+  create_service_linked_role_spot   = true
+  enable_ssm_on_runners             = true
+  instance_types                    = "t2.small"
+  sqs_build_queue                   = aws_sqs_queue.runner-queue.name
+  runner_iam_role_managed_policy_arns = "arn:aws:iam::490638925706:role/SSMInstanceProfile"
   webhook_lambda_zip                = "lambdas-download/webhook.zip"
   runner_binaries_syncer_lambda_zip = "lambdas-download/runner-binaries-syncer.zip"
   runners_lambda_zip                = "lambdas-download/runners.zip"
+}
+
+resource "aws_sqs_queue" "runner-queue" {
+  name                      = "runner-queue"
+  redrive_policy = jsonencode({
+    deadLetterTargetArn = aws_sqs_queue.terraform_queue_deadletter.arn
+    maxReceiveCount     = 4
+  })
+
+  tags = {
+    Environment = "prod"
+  }
+}
+
+resource "aws_sqs_queue" "terraform_queue_deadletter" {
+  name                      = "deadletter-queue"
+  tags = {
+    Environment = "prod"
+  }
 }
