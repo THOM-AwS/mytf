e795ab09a4ba2f6119cc661c1d5b8ae39bad2fcd
Thomas Hamer <thomas_hamer@outlook.com>
2021-09-26 19:53:24 +1000

    add extra workflows for wcplumbing

--- websites/locals.tf
+++ websites/locals.tf
@@ -6,6 +6,11 @@ locals {
       aws_region  = "us-east-1"
       domain_name = "hamer.cloud"
     }
+    wcplumbing = {
+      aws_profile = "hamer"
+      aws_region  = "us-east-1"
+      domain_name = "wcplumbing.com.au"
+    }
   }
 
   workspace = local.env[terraform.workspace]
