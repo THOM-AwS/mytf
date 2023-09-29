cbe61676892cdc8f4069298a9001f86913743a10
Thomas Hamer <thomas_hamer@outlook.com>
2023-09-29 22:06:13 +1000

    incremental api

--- websites/apig_responses.tf
+++ websites/apig_responses.tf
@@ -1,16 +1,72 @@
-# / path
-resource "aws_api_gateway_integration_response" "root_mock_200_response" {
-  depends_on  = [aws_api_gateway_integration.root_mock_integration]
+# ===========================================
+# API Gateway Method Responses
+# ===========================================
+
+# Root Path Method Response
+
+resource "aws_api_gateway_method_response" "root_get_200" {
   rest_api_id = aws_api_gateway_rest_api.generic_api.id
   resource_id = aws_api_gateway_rest_api.generic_api.root_resource_id
   http_method = aws_api_gateway_method.root_get.http_method
   status_code = "200"
+  response_parameters = {
+    "method.response.header.Content-Type" = true
+  }
+  response_models = {
+    "application/json" = aws_api_gateway_model.response_model.name
+  }
+}
 
+# /data Path Method Responses
+
+resource "aws_api_gateway_method_response" "data_post_200" {
+  rest_api_id = aws_api_gateway_rest_api.generic_api.id
+  resource_id = aws_api_gateway_resource.generic_resource.id
+  http_method = aws_api_gateway_method.generic_post.http_method
+  status_code = "200"
+  response_parameters = {
+    "method.response.header.Content-Type" = true
+  }
+}
+
+resource "aws_api_gateway_method_response" "data_post_400" {
+  rest_api_id       = aws_api_gateway_rest_api.generic_api.id
+  resource_id       = aws_api_gateway_resource.generic_resource.id
+  http_method       = aws_api_gateway_method.generic_post.http_method
+  status            = "400"
+  selection_pattern = "4\\d{2}"
+  response_models = {
+    "application/json" = "Client Error"
+  }
+}
+
+resource "aws_api_gateway_method_response" "data_post_500" {
+  rest_api_id       = aws_api_gateway_rest_api.generic_api.id
+  resource_id       = aws_api_gateway_resource.generic_resource.id
+  http_method       = aws_api_gateway_method.generic_post.http_method
+  status            = "500"
+  selection_pattern = "5\\d{2}"
+  response_models = {
+    "application/json" = "Server Error"
+  }
+}
+
+# ===========================================
+# API Gateway Integration Responses
+# ===========================================
+
+# Root Path Integration Response
+
+resource "aws_api_gateway_integration_response" "root_mock_200_response" {
+  depends_on        = [aws_api_gateway_integration.root_mock_integration]
+  rest_api_id       = aws_api_gateway_rest_api.generic_api.id
+  resource_id       = aws_api_gateway_rest_api.generic_api.root_resource_id
+  http_method       = aws_api_gateway_method.root_get.http_method
+  status_code       = "200"
+  selection_pattern = "2\\d{2}"
   response_parameters = {
     "method.response.header.Content-Type" = "'application/json'"
-    // Add other headers if necessary
   }
-
   response_templates = {
     "application/json" = jsonencode({
       statusCode = 200,
@@ -19,33 +75,18 @@ resource "aws_api_gateway_integration_response" "root_mock_200_response" {
   }
 }
 
-# / path
-resource "aws_api_gateway_method_response" "root_get_200" {
-  rest_api_id = aws_api_gateway_rest_api.generic_api.id
-  resource_id = aws_api_gateway_rest_api.generic_api.root_resource_id
-  http_method = aws_api_gateway_method.root_get.http_method
-  status_code = "200"
+# /data Path Integration Responses
 
-  response_parameters = {
-    "method.response.header.Content-Type" = true
-    // Add other headers if necessary
-  }
-  response_models = {
-    "application/json" = aws_api_gateway_model.response_model.name
-  }
-}
-
-# /data path
-resource "aws_api_gateway_integration_response" "ddb_200_response" {
-  depends_on  = [aws_api_gateway_integration.ddb_integration]
-  rest_api_id = aws_api_gateway_rest_api.generic_api.id
-  resource_id = aws_api_gateway_resource.generic_resource.id
-  http_method = aws_api_gateway_method.generic_post.http_method
-  status_code = "200"
+resource "aws_api_gateway_integration_response" "data_post_200_response" {
+  depends_on        = [aws_api_gateway_integration.ddb_integration]
+  rest_api_id       = aws_api_gateway_rest_api.generic_api.id
+  resource_id       = aws_api_gateway_resource.generic_resource.id
+  http_method       = aws_api_gateway_method.generic_post.http_method
+  status_code       = "200"
+  selection_pattern = "2\\d{2}"
   response_parameters = {
     "method.response.header.Content-Type" = "'application/json'"
   }
-
   response_templates = {
     "application/json" = <<-EOF
 {
@@ -61,86 +102,47 @@ EOF
   }
 }
 
-# / path
-resource "aws_api_gateway_method_response" "generic_post_200" {
-  rest_api_id = aws_api_gateway_rest_api.generic_api.id
-  resource_id = aws_api_gateway_resource.generic_resource.id
-  http_method = aws_api_gateway_method.generic_post.http_method
-  status_code = "200"
-
-  response_parameters = {
-    "method.response.header.Content-Type" = true
-  }
-}
-# /data path
-resource "aws_api_gateway_integration_response" "ddb_4XX_response" {
-  rest_api_id = aws_api_gateway_rest_api.generic_api.id
-  resource_id = aws_api_gateway_resource.generic_resource.id
-  http_method = aws_api_gateway_method.generic_post.http_method
-  status      = "400"
-
+resource "aws_api_gateway_integration_response" "data_post_4XX_response" {
+  rest_api_id       = aws_api_gateway_rest_api.generic_api.id
+  resource_id       = aws_api_gateway_resource.generic_resource.id
+  http_method       = aws_api_gateway_method.generic_post.http_method
+  status            = "400"
+  selection_pattern = "4\\d{2}"
   response_templates = {
     "application/json" = "{\"error\": \"Client Error\"}"
   }
-
-  selection_pattern = "4\\d{2}" # Matches any 400-series error from DynamoDB
 }
 
-# /data path
-resource "aws_api_gateway_integration_response" "ddb_5XX_response" {
-  rest_api_id = aws_api_gateway_rest_api.generic_api.id
-  resource_id = aws_api_gateway_resource.generic_resource.id
-  http_method = aws_api_gateway_method.generic_post.http_method
-  status      = "500"
-
+resource "aws_api_gateway_integration_response" "data_post_5XX_response" {
+  rest_api_id       = aws_api_gateway_rest_api.generic_api.id
+  resource_id       = aws_api_gateway_resource.generic_resource.id
+  http_method       = aws_api_gateway_method.generic_post.http_method
+  status            = "500"
+  selection_pattern = "5\\d{2}"
   response_templates = {
     "application/json" = "{\"error\": \"Server Error\"}"
   }
-
-  selection_pattern = "5\\d{2}" # Matches any 500-series error from DynamoDB
 }
 
+# ===========================================
+# Default API Gateway Responses
+# ===========================================
 
-resource "aws_api_gateway_method_response" "400_response" {
-  rest_api_id = aws_api_gateway_rest_api.generic_api.id
-  resource_id = aws_api_gateway_resource.generic_resource.id
-  http_method = aws_api_gateway_method.generic_post.http_method
-  status      = "400"
-
-  response_models = {
-    "application/json" = "Error"
-  }
-}
-
-resource "aws_api_gateway_method_response" "500_response" {
-  rest_api_id = aws_api_gateway_rest_api.generic_api.id
-  resource_id = aws_api_gateway_resource.generic_resource.id
-  http_method = aws_api_gateway_method.generic_post.http_method
-  status      = "500"
-
-  response_models = {
-    "application/json" = "Error"
-  }
-}
-
-resource "aws_api_gateway_gateway_response" "default_4xx" {
+resource "aws_api_gateway_gateway_response" "default_4xx_response" {
   rest_api_id   = aws_api_gateway_rest_api.generic_api.id
+  status        = 400
   response_type = "DEFAULT_4XX"
-
   response_templates = {
     "application/json" = "{\"message\": \"$context.error.message\"}"
   }
-
-  status = 400
 }
 
-resource "aws_api_gateway_gateway_response" "default_5xx" {
+resource "aws_api_gateway_gateway_response" "default_5xx_response" {
   rest_api_id   = aws_api_gateway_rest_api.generic_api.id
+  status        = 500
   response_type = "DEFAULT_5XX"
-
   response_templates = {
     "application/json" = "{\"message\": \"An unexpected error occurred.\"}"
   }
-
-  status = 500
 }
+
