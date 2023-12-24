d9a533c431e8177c90d22a0386827752f2079333
Thomas Hamer <thomas_hamer@outlook.com>
2023-12-24 15:31:27 +1100

    refactor and improve lambda

--- Makefile
+++ Makefile
@@ -12,6 +12,7 @@ init: .env
 plan: .env init workspace
 	docker-compose run --rm envvars ensure --tags terraform
 	docker-compose run --rm terraform-utils sh -c 'cd $${SUBFOLDER}; terraform plan'
+	docker-compose run --rm terraform-utils sh -c 'rm -rf .terraform/modules/'
 .PHONY: plan
 
 validate: .env
@@ -27,11 +28,15 @@ output: .env init workspace
 apply: .env init workspace
 	docker-compose run --rm envvars ensure --tags terraform
 	docker-compose run --rm terraform-utils sh -c 'cd $${SUBFOLDER}; terraform apply'
+	docker-compose run --rm terraform-utils sh -c 'rm -rf websites/.terraform/modules/'
+	docker-compose run --rm terraform-utils sh -c 'rm -rf websites/secheader.py.zip'
 .PHONY: apply
 
 applyAuto: .env init workspace
 	docker-compose run --rm envvars ensure --tags terraform
 	docker-compose run --rm terraform-utils sh -c 'cd $${SUBFOLDER}; terraform apply -auto-approve'
+	docker-compose run --rm terraform-utils sh -c 'rm -rf websites/.terraform/modules/'
+	docker-compose run --rm terraform-utils sh -c 'rm -rf websites/secheader.py.zip'
 .PHONY: applyAuto
 
 destroy: .env init workspace
--- websites/lambdaGet.py
+++ websites/lambdaGet.py
@@ -34,8 +34,48 @@ def handler(event, context):
 
     # Fallback item if no items are found
     if len(items) < 1:
-        items = [{"lat": -33.8688, "lon": 151.2093, "timestamp": 1700267130}]
-        logger.info("Using fallback location")
+        items = [
+            {
+                "lat": -33.8707665, 
+                "lon": 151.1996391, 
+                "timestamp": 1700000000
+            },
+            {
+                "lat": -33.8702265,
+                "lon": 151.1994001, 
+                "timestamp": 1700000001
+            },
+            {
+                "lat": -33.8710335,
+                "lon": 151.1965441,
+                "timestamp": 1700000002
+            },
+            {
+                "lat": -33.8734745,
+                "lon": 151.1977241,
+                "timestamp": 1700000003
+            },
+            {
+                "lat": -33.8731355, 
+                "lon": 151.1990331,
+                "timestamp": 1700000004
+            },
+            {
+                "lat": -33.8702154,
+                "lon": 151.2009972,
+                "timestamp": 1700000005
+            },
+            {
+                "lat": -33.8681989,
+                "lon": 151.2007921,
+                "timestamp": 1700000006
+            },
+            {
+                "lat": -33.8707665, 
+                "lon": 151.1996391, 
+                "timestamp": 1700000007
+            }
+        ]
 
     return {
         'statusCode': 200,
