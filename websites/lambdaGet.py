a4601465d7b371a65e73f3e69bd4ba6011b70e63
Thomas Hamer <thomas_hamer@outlook.com>
2023-12-28 12:54:41 +1100

    update default radius

--- websites/lambdaGet.py
+++ websites/lambdaGet.py
@@ -21,7 +21,7 @@ def handler(event, context):
     try:
         response = table.scan()
         items = response.get('Items', [])
-        logger.info("Items retrieved: %s", items)
+        # logger.info("Items retrieved: %s", items)
     except Exception as e:
         logger.error("Error during DynamoDB scan: %s", e)
         return {
@@ -33,95 +33,97 @@ def handler(event, context):
             'body': json.dumps({'message': 'Internal Server Error'})
         }
 
-    current_timestamp = int(time.time())
-    for i, item in enumerate(items):
-        item['timestamp'] = (current_timestamp - 15) + i
+    
 
     # Fallback item if no items are found
     if len(items) < 1:
+        current_timestamp = int(time.time())
+        for i, item in enumerate(items):
+            item['timestamp'] = (current_timestamp - 15) + i
+            
         items = [
             {
                 "lat": -33.87076,
                 "lon": 151.20219,
                 "alt": 20,
-                "acc": 30,
+                "acc": 2.0,
                 "bat": 100,
                 "sat": 0,
                 "useragent": "Bot",
                 "speed": 1,
-                "bearing": 270,
+                "bearing": 270.0,
                 "timestamp": current_timestamp
             },
             {
                 "lat": -33.87070,
                 "lon": 151.20140,
                 "alt": 20,
-                "acc": 30,
+                "acc": 2.0,
                 "bat": 100,
                 "sat": 0,
                 "useragent": "Bot",
                 "speed": 1,
-                "bearing": 270,
+                "bearing": 270.0,
                 "timestamp": current_timestamp
             },
             {
                 "lat": -33.87045,
                 "lon": 151.19999,
                 "alt": 20,
-                "acc": 30,
+                "acc": 2.0,
                 "bat": 100,
                 "sat": 0,
                 "useragent": "Bot",
                 "speed": 1,
-                "bearing": 270,
+                "bearing": 270.0,
                 "timestamp": current_timestamp
             },
             {
                 "lat": -33.87022,
                 "lon": 151.19917,
                 "alt": 20,
-                "acc": 30,
+                "acc": 2.0,
                 "bat": 100,
                 "sat": 270,
                 "useragent": "Bot",
                 "speed": 1,
-                "bearing": 180,
+                "bearing": 180.0,
                 "timestamp": current_timestamp
             },
             {
                 "lat": -33.87105,
                 "lon": 151.19921,
                 "alt": 20,
-                "acc": 30,
+                "acc": 2.0,
                 "bat": 100,
                 "sat": 0,
                 "useragent": "Bot",
                 "speed": 1,
-                "bearing": 120,
+                "bearing": 120.0,
                 "timestamp": current_timestamp
             },
             {
                 "lat": -33.871271,
                 "lon": 151.199504,
                 "alt": 20,
-                "acc": 30,
+                "acc": 2.0,
                 "bat": 100,
                 "sat": 0,
                 "useragent": "Bot",
                 "speed": 1,
-                "bearing": 180,
+                "bearing": 180.0,
                 "timestamp": current_timestamp
             },
             {
                 "lat": -33.87220,
                 "lon": 151.19944,
                 "alt": 20,
-                "acc": 30,
+                "acc": 2.0,
                 "bat": 100,
                 "sat": 0,
                 "useragent": "Bot",
                 "speed": 1,
-                "bearing": 180,
+                "bearing": 180.0,
                 "timestamp": current_timestamp
             },
             
@@ -129,12 +131,12 @@ def handler(event, context):
                 "lat": -33.87310,
                 "lon": 151.20000,
                 "alt": 20,
-                "acc": 30,
+                "acc": 2.0,
                 "bat": 100,
                 "sat": 0,
                 "useragent": "Bot",
                 "speed": 1,
-                "bearing": 180,
+                "bearing": 180.0,
                 "timestamp": current_timestamp
             },
             
@@ -142,12 +144,12 @@ def handler(event, context):
                 "lat": -33.87350,
                 "lon": 151.20000,
                 "alt": 20,
-                "acc": 30,
+                "acc": 2.0,
                 "bat": 100,
                 "sat": 120,
                 "useragent": "Bot",
                 "speed": 1,
-                "bearing": 120,
+                "bearing": 120.0,
                 "timestamp": current_timestamp
             },
             
@@ -155,24 +157,24 @@ def handler(event, context):
                 "lat": -33.87349,
                 "lon": 151.20050,
                 "alt": 20,
-                "acc": 30,
+                "acc": 2.0,
                 "bat": 100,
                 "sat": 0,
                 "useragent": "Bot",
                 "speed": 1,
-                "bearing": 90,
+                "bearing": 90.0,
                 "timestamp": current_timestamp
             },
             {
                 "lat": -33.873310,
                 "lon": 151.2012261,
                 "alt": 20,
-                "acc": 30,
+                "acc": 2.0,
                 "bat": 100,
                 "sat": 0,
                 "useragent": "Bot",
                 "speed": 1,
-                "bearing": 70,
+                "bearing": 70.0,
                 "timestamp": current_timestamp
             },
             
@@ -180,43 +182,43 @@ def handler(event, context):
                 "lat": -33.87317,
                 "lon": 151.20187,
                 "alt": 20,
-                "acc": 30,
+                "acc": 2.0,
                 "bat": 100,
                 "sat": 0,
                 "useragent": "Bot",
                 "speed": 1,
-                "bearing": 1,
+                "bearing": 1.0,
                 "timestamp": current_timestamp
             },
             {
                 "lat": -33.87215,
                 "lon": 151.20190,
                 "alt": 20,
-                "acc": 30,
+                "acc": 2.0,
                 "bat": 100,
                 "sat": 0,
                 "useragent": "Bot",
                 "speed": 1,
-                "bearing": 1,
+                "bearing": 1.0,
                 "timestamp": current_timestamp
             },
             {
                 "lat": -33.8713138,
                 "lon": 151.2020507,
                 "alt": 20,
-                "acc": 30,
+                "acc": 2.0,
                 "bat": 100,
                 "sat": 0,
                 "useragent": "Bot",
                 "speed": 1,
-                "bearing": 1,
+                "bearing": 1.0,
                 "timestamp": current_timestamp
             },
             {
                 "lat": -33.87076,
                 "lon": 151.20219,
                 "alt": 20,
-                "acc": 30,
+                "acc": 2.0,
                 "bat": 100,
                 "sat": 0,
                 "useragent": "Bot",
