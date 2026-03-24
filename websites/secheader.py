de53ddcbfb7a989524bfbbfca344e81db5bb6dfb
Thomas Hamer <thomas_hamer@outlook.com>
2026-03-25 00:05:16 +1100

    fix: CSP frame-src allow self for embedded family tree iframe
    - Add 'self' to frame-src to allow family-tree.html iframe in index.html
    - Add google.co.jp to img-src for GA audiences pixel

--- websites/secheader.py
+++ websites/secheader.py
@@ -28,12 +28,12 @@ def lambda_handler(event, context):
         security_headers = [
             {'key': 'content-security-policy', 'value': (
                 "default-src 'self'; "
-                "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://www.google.com https://static.doubleclick.net https://www.youtube.com https://s.ytimg.com https://cdn.jsdelivr.net https://unpkg.com https://maps.googleapis.com https://balkan.app; "
+                "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://www.google.com https://static.doubleclick.net https://www.youtube.com https://s.ytimg.com https://cdn.jsdelivr.net https://unpkg.com https://maps.googleapis.com; "
                 "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://www.youtube.com https://cdnjs.cloudflare.com; "
-                "img-src 'self' data: https://avatars.githubusercontent.com https://i.ytimg.com https://yt3.ggpht.com https://www.googletagmanager.com https://www.google.com.au https://www.google-analytics.com https://maps.googleapis.com https://maps.gstatic.com https://khms0.googleapis.com https://khms1.googleapis.com; "
+                "img-src 'self' data: https://avatars.githubusercontent.com https://i.ytimg.com https://yt3.ggpht.com https://www.googletagmanager.com https://www.google.com.au https://www.google-analytics.com https://analytics.google.com https://stats.g.doubleclick.net https://maps.googleapis.com https://maps.gstatic.com https://khms0.googleapis.com https://khms1.googleapis.com https://www.google.co.jp; "
                 "font-src 'self' https://fonts.gstatic.com https://cdnjs.cloudflare.com; "
-                "connect-src 'self' https://www.googletagmanager.com https://www.google.com https://www.youtube.com https://play.google.com https://api.hamer.cloud https://api.github.com https://maps.googleapis.com; "
-                "frame-src https://www.youtube.com https://cdn.jsdelivr.net; "
+                "connect-src 'self' https://www.googletagmanager.com https://www.google.com https://www.google.com.au https://www.google-analytics.com https://analytics.google.com https://stats.g.doubleclick.net https://www.youtube.com https://play.google.com https://api.hamer.cloud https://api.github.com https://maps.googleapis.com https://raw.githubusercontent.com; "
+                "frame-src 'self' https://www.youtube.com https://cdn.jsdelivr.net; "
                 "object-src 'none'; "
                 "form-action 'self'; "
                 "frame-ancestors 'none'; "
